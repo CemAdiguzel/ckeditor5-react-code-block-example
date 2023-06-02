a7e21c284c11af331d5713bce95119ddb9461836
CemAdiguzel <cemadiguzel1993@gmail.com>
2023-06-02 13:23:44 +0300

    finalize editor

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,157 @@ import { Essentials } from "@ckeditor/ckeditor5-essentials";
 import { Bold, Italic } from "@ckeditor/ckeditor5-basic-styles";
 import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
 import { CodeBlock } from "@ckeditor/ckeditor5-code-block";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { AutoLink, Link } from "@ckeditor/ckeditor5-link";
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat";
+import { List } from "@ckeditor/ckeditor5-list";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import { Indent, IndentBlock } from "@ckeditor/ckeditor5-indent";
+import { Table, TableToolbar } from "@ckeditor/ckeditor5-table";
+import { Undo } from "@ckeditor/ckeditor5-undo";
+import {
+  Image,
+  ImageCaption,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  ImageResizeEditing,
+  ImageResizeHandles,
+  ImageResizeButtons,
+} from "@ckeditor/ckeditor5-image";
 
 const editorConfiguration = {
-  plugins: [Essentials, Bold, Italic, Paragraph, CodeBlock],
-  toolbar: ["bold", "italic", "codeBlock"],
+  plugins: [
+    Essentials,
+    Bold,
+    Italic,
+    Paragraph,
+    CodeBlock,
+    Heading,
+    AutoLink,
+    Link,
+    Autoformat,
+    List,
+    BlockQuote,
+    Indent,
+    IndentBlock,
+    Table,
+    TableToolbar,
+    Undo,
+    Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    ImageResizeEditing,
+    ImageResizeHandles,
+    ImageResizeButtons,
+  ],
+  extraPlugins: [MyCustomUploadAdapterPlugin],
+  toolbar: [
+    "heading",
+    "|",
+    "bold",
+    "italic",
+    "link",
+    "bulletedList",
+    "numberedList",
+    "|",
+    "codeBlock",
+    "blockQuote",
+    "indent",
+    "outdent",
+    "insertTable",
+    "|",
+    "undo",
+    "redo",
+    "|",
+    "imageUpload",
+    "imageStyle:inline",
+    "imageStyle:alignLeft",
+    "imageStyle:alignRight",
+    "imageStyle:alignCenter",
+    "imageStyle:alignBlockLeft",
+    "imageStyle:alignBlockRight",
+  ],
+  image: {
+    resizeOptions: [
+      {
+        name: "resizeImage:original",
+        value: null,
+        icon: "original",
+      },
+      {
+        name: "resizeImage:50",
+        value: "50",
+        icon: "medium",
+      },
+      {
+        name: "resizeImage:75",
+        value: "75",
+        icon: "large",
+      },
+    ],
+    toolbar: [
+      "resizeImage:50",
+      "resizeImage:75",
+      "resizeImage:original",
+      // More toolbar options.
+      // ...
+    ],
+  },
+  table: {
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+  },
+  link: {
+    addTargetToExternalLinks: true,
+  },
 };
+class MyUploadAdapter {
+  constructor(loader) {
+    this.loader = loader;
+  }
+
+  // Starts the upload process.
+  upload() {
+    return this.loader.file.then(
+      (file) =>
+        new Promise((resolve, reject) => {
+          const cloudName = "hireg";
+          const unsignedUploadPreset = "rwjokdvu";
+          const data = new FormData();
+          data.append("file", file);
+          data.append("upload_preset", unsignedUploadPreset);
+          data.append("max_width", 3000);
+          data.append("folder", "UserUploads");
+          fetch(`https://api.cloudinary.com/v1_1/${cloudName}/upload`, {
+            body: data,
+            header: {
+              "X-Requested-With": "XMLHttpRequest",
+            },
+            method: "POST",
+          })
+            .then((response) => {
+              return response.json();
+            })
+            .then((file) => {
+              const resData = file;
+              resData.default = file.url;
+              resolve(resData);
+            });
+        })
+    );
+  }
+}
+
+function MyCustomUploadAdapterPlugin(editor) {
+  editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+    // Configure the URL to the upload script in your back-end here!
+
+    console.log(Array.from(editor.ui.componentFactory.names));
+    return new MyUploadAdapter(loader);
+  };
+}
 
 class App extends Component {
   render() {
